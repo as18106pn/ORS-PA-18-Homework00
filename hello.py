f71e36d5a16d90f89e364607998a700601a9fe11
Almira Suljovic <almira.suljovic@udg.edu.me>
2019-02-11 15:09:28 +0100

    Add rad1

--- hello.py
+++ hello.py
@@ -1,9 +1,15 @@
+def validni_brojevi(x,y):
+    if not x.isnumeric() or not y.isnumeric():
+        return False
+    else:
+        return True
+
+
 korisnik_unio_prvi = input("Unesite prvi broj: ")
 korisnik_unio_drugi = input("Unesite drugi broj: ")
 
-if (not korisnik_unio_prvi.isnumeric()) or (not korisnik_unio_drugi.isnumeric()) :
-    print("Nijeste unijeli ispravne brojeve")
-    quit()
+if not validni_brojevi(korisnik_unio_prvi,korisnik_unio_drugi):
+    quit("Brojevi nisu validni")
 
 broj_prvi = int(korisnik_unio_prvi)
 broj_drugi = int(korisnik_unio_drugi)
